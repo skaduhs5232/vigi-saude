02d47b27d4d64886e76528c0e085bb6a8b7ede71
Gabriela Abreu <ga.abreu@gmail.com>
2025-10-24 20:08:44 -0300

    feat: Criação de endpoints para indicadores

---
+++ VigiSaude.Backend/Models/DTO/Indicadores/EstatisticasSetorTipoIncidenteDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace VigiSaude.Backend.Models.DTO.Indicadores
+{
+    public class EstatisticaSetorTipoIncidenteMensalDTO
+    {
+        public string Setor { get; set; } = string.Empty;
+        public string TipoIncidente { get; set; } = string.Empty;
+        public string MesAno { get; set; } = string.Empty; 
+        public int Quantidade { get; set; }
+    }
+}
+
